94bbda382bbc2bb8ed00f95a8b2d13b9197f9f86
Vladislav <drimacus182@gmail.com>
2018-01-22 17:39:01 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    var template = "[<<link_text>>](https://t.me/iv?url=<<link_href>>&rhash=5a55dde839c9a5)";
+        
+    $('#input_link_href').on('input change keyup', onChange)
+    $('#input_link_text').on('input change keyup', onChange)
+
+    $('#input_link_href').trigger('change');
+
+    function onChange() {
+        try {           
+            var link_text = $('#input_link_text').val().trim();
+            var link_href = $('#input_link_href').val().trim();
+            
+            var output = template
+                .replace("<<link_text>>", link_text)
+                .replace("<<link_href>>", link_href);
+            
+            $('#output').text(output);
+        } catch (err) {
+            $('#output').text('');
+            console.log(err);
+        }
+    }
+});
